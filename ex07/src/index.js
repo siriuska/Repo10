c0a9009a9a485d8aa58b37ddecee5bce0f8ee11b
siriuska <you@example.com>
2022-04-06 17:32:06 +0200

    Delete properties from an object

---
+++ ex07/src/index.js
@@ -0,0 +1,14 @@
+function myFunction(propName) {
+    var lion = {
+        name: "Simba",
+        legs: 4,
+        tails: 1,
+        roar: "roar-roar"
+    }
+    delete lion.roar;
+    return lion;
+}
+
+console.log(myFunction("roar"));
+
+module.exports = myFunction;
